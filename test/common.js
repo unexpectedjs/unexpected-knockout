6b9edd86d3a553de24e8205222f7680d52d65100
Sune Simonsen <sune@we-knowhow.dk>
2015-05-06 23:54:59 +0200

    First documentation site (hand rolled)

--- test/common.js
+++
@@ -1,5 +0,0 @@
-ko = require('knockout');
-expect = require('unexpected');
-unexpectedKnockout = process.env.COVERAGE ?
-    require('../lib-cov/unexpected-knockout') :
-    require('../lib/unexpected-knockout');
