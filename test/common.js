e4f27daf9c84ddf90f50e01319108ad6da28d8e6
Andreas Lind <andreaslindpetersen@gmail.com>
2019-01-05 01:21:31 +0100

    Remove lib-cov artifacts

--- test/common.js
+++ test/common.js
@@ -1,9 +1,7 @@
 /* global unexpected:true , ko:true, unexpectedKnockout:true */
 /* exported ko */
 ko = require('knockout');
-unexpectedKnockout = process.env.COVERAGE
-  ? require('../lib-cov/unexpected-knockout')
-  : require('../lib/unexpected-knockout');
+unexpectedKnockout = require('../lib/unexpected-knockout');
 unexpected = require('unexpected')
   .clone()
   .installPlugin(unexpectedKnockout);
